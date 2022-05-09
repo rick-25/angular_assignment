7ed73a27ca52cc2fa11a2a9291d486704fd74fdc
Aditya Kumar <araj7060@gmail.com>
2022-05-09 09:03:32 +0530

    Added components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { FormHeaderComponent } from './form-header/form-header.component';
+import { FormMainComponent } from './form-main/form-main.component';
+import { FormFooterComponent } from './form-footer/form-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormHeaderComponent,
+    FormMainComponent,
+    FormFooterComponent
   ],
   imports: [
     BrowserModule
