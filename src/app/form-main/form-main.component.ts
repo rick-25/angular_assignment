9cdd3bca898853ad2b25ae3011cd8190af71d833
Aditya Kumar <araj7060@gmail.com>
2022-05-09 11:52:57 +0530

    added styles

--- src/app/form-main/form-main.component.ts
+++ src/app/form-main/form-main.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Question {
+  id: number;
+  text: string;
+  required: boolean;
+  valueType: string;
+  selectedValue?: string | boolean;
+}
+
 @Component({
   selector: 'app-form-main',
   templateUrl: './form-main.component.html',
   styleUrls: ['./form-main.component.css']
 })
 export class FormMainComponent implements OnInit {
+
+  questions?: Question[] = [
+    {
+      id: 1,
+      text: 'Are you gay',
+      required: true,
+      valueType: 'text'
+    }
+  ];
 
   constructor() { }
 
